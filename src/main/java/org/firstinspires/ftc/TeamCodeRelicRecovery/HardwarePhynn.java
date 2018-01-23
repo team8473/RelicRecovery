66cdf4bd76615f5f90784c68edf0fbba6e0c897f
team8473 <gabriel.mahlen@egpg.org>
2018-01-22 19:08:18 -0600

    Updated HardwarePhynn

--- src/main/java/org/firstinspires/ftc/TeamCodeRelicRecovery/HardwarePhynn.java
+++ src/main/java/org/firstinspires/ftc/TeamCodeRelicRecovery/HardwarePhynn.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class HardwarePhynn {
 
@@ -15,6 +16,24 @@ public class HardwarePhynn {
     public Servo servo2 = null;
     public Servo servo3 = null;
     public ColorSensor ColorSensor = null;
+
+    public ElapsedTime runtime = new ElapsedTime();
+
+    public static final double     COUNTS_PER_MOTOR_REV    = 374 ;      // eg: ANDY MARK Motor Encoder
+    public static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    public static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference of the wheel
+    public static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    public static final double     DRIVE_SPEED             = 0.25;
+    public static final double     TURN_SPEED              = 0.4;
+    public static final double     RIGHT                   = 1;
+    public static final double     LEFT                    = 0;
+    public static final double     ROBOT_SHORT_DIAMETER_IN = 16.35; // Used to find the circumference of the robots ellipse
+    public static final double     ROBOT_LONG_DIAMETER_IN  = 17.4;  // Used to find the circumference of the robots ellipse
+
+    public static final double     kP                      = 0;
+    public static final double     kI                      = 0;
+    public static final double     kD                      = 0;
 
     boolean Open = true;
     boolean See = true;
