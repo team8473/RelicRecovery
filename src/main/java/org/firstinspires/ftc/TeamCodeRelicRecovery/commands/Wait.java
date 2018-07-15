dfb8f92caa32937f8e3566abe44ae921749509b6
team8473 <gabriel.mahlen@egpg.org>
2018-07-15 18:33:46 -0500

    Fixed errors with the grabbers

--- src/main/java/org/firstinspires/ftc/TeamCodeRelicRecovery/commands/Wait.java
+++ src/main/java/org/firstinspires/ftc/TeamCodeRelicRecovery/commands/Wait.java
@@ -22,4 +22,11 @@ public class Wait extends LinearOpMode {
             telemetry.addData("Time : ", timer.milliseconds());
         }
     }
+
+    public void waitSeconds(double time) {
+        timer.reset();
+        while(timer.seconds() <= time){
+            telemetry.addData("Time : ", timer.seconds());
+        }
+    }
 }
